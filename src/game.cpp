c747dc7dc6901c33b8d58a3aab439514e6a28da1
CPlusPlusNerd <54821768+CPlusPlusNerd@users.noreply.github.com>
2021-01-28 12:26:44 -0700

    New Background Color
    To be honest I would prefer a black background over a blue one that burns my eyes out.

--- src/game.cpp
+++ src/game.cpp
@@ -65,7 +65,7 @@ void Game::update() {
 
 void Game::output() {
 
-  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_Rect wall{0, 0, 1024, thickness};
